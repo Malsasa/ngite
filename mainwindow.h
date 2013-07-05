a0f33fed089bdc3221eda43cd402ae707219de50
Malsasa <teknoloid@gmail.com>
2013-07-05 21:51:23 +0700

    Create mainwindow.h

---
+++ mainwindow.h
@@ -0,0 +1,19 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QDialog>
+
+class QDialog;
+
+class Dialog : public QDialog
+{
+    Q_OBJECT
+    
+public:
+    Dialog();
+    
+private:
+
+};
+
+#endif // MAINWINDOW_H
